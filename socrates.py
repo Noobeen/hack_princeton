e33ab8ab8e28cc41ef5045f3e075fdc906db684e
PrameyaDhdl <prameyadhaubhadel@gmail.com>
2024-11-10 07:50:04 -0500

    Update socrates.py
    1

--- socrates.py
+++ socrates.py
@@ -147,7 +147,7 @@ conversational_rag_chain = RunnableWithMessageHistory(
 
 
 ## Passing to streamlit and running our chatbot 
-st.title("Herschel")
+st.title("Socrates")
 Prompt= st.chat_input("Enter you queries here : ")# taking prompt for the user
 if Prompt:
     st.write(f"You have sent following prompt: {Prompt}")
